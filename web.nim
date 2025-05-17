e662de10e30710e8d87bd0835e4032d33919b2c7
Cristobal <cristobalriaga@gmail.com>
2025-05-16 20:52:17 -0500

    note to allow local file cors on firefox, moved datos path information into its own variable

--- web.nim
+++ web.nim
@@ -1,3 +1,4 @@
+# NOTE: to allow ./index.html to load a local file in firefox 106+, set security.fileuri.strict_origin_policy to false in about:config
 import karax/[karax, karaxdsl, vdom]
 import std/[dom, jsconsole, times, jsfetch, asyncjs]
 import entry
@@ -17,18 +18,21 @@ type
     failure
     fetching
 
+const homeDir = "/home/cristobal"
+# const homeDir = "/home/carlosriaga/"
+const datosPath = homeDir & "/clones/aves/datos.json"
+# const datosPath = homeDir & "Documents/nim/fotos%20aves/datos.json"
 var state = State()
 
 proc parseHook*(s: string, i: var int, v: var DateTime) =
   var str: string
-  echo (s: s, i: i, v: v)
-  #parseHook(s, i, str)
-  #v = parse(str, "yyyy-MM-dd hh:mm:ss")
+  # echo (s: s, i: i, v: v)
+  parseHook(s, i, str)
+  v = parse(str, "yyyy-MM-dd hh:mm:ss")
 
 proc fetchData(state: var State) {.async.} =
   state.fetchState = fetching
-  let response =
-    await fetch cstring"file:///home/carlosriaga/Documents/nim/fotos%20aves/datos.json"
+  let response = await fetch datosPath.cstring
 
   if response.ok:
     state.data = fromJson($response.body, seq[Entry])
